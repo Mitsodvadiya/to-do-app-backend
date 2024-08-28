302e162fa85b620b039b4f687bccce912453d1a2
meet <meetsodvadiya004@gmail.com>
2024-08-28 09:12:01 +0530

    .env fixed..

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 const port = process.env.PORT || 8000
 const taskRouter = require('./routes/task.router')
 const connectDB = require('./config/db');
-const dotenv = require('dotenv').config();
 const path = require('path');
-console.log(process.env.PORT)
 connectDB();
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
